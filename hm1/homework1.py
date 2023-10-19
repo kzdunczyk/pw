f1a60f9e0f1e72c23bd1a01ac1129330ed494f3d
Kajetan Zduńczyk <262837@student.pwr.edu.pl>
2023-10-19 22:53:28 +0200

    accuracy function

--- hm1/homework1.py
+++ hm1/homework1.py
@@ -1,31 +1,50 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
-
+import matplotlib.pyplot as plt
 
 #przygotowanie danych
 
-X = pd.read_csv("../hm1/X.csv")
-y = pd.read_csv("../hm1/y.csv")
+X = pd.read_csv("C:/Users/Kajetan/Desktop/pw/hm1/X.csv")
+y = pd.read_csv("C:/Users/Kajetan/Desktop/pw/hm1/y.csv")
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 335723)
-
-def accuracy(n_splits,X, y, kryt: str):
-    if (n_splits <5):
+def accuracy(cv:int,X, y, wart = 0):
+    if(cv <5):
         return "Podaj wartosc wieksza niz 5"
     else:
-        if kryt == 'gini'or kryt == 'entropy':
-            #kryterium podziału gini lub entropia
-            Tree = tree.DecisionTreeClassifier(criterion = kryt)  
-            Tree.fit(X_train, y_train)
-            kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=None)
-            y_pred = Tree.predict(X_test)
-            accuracy = accuracy_score(y_test, y_pred)
-            return accuracy
-
-entropia = accuracy(8,X_test,y_test,kryt = 'entropy')
-gini = accuracy(8,X_test,y_test,kryt = 'gini')
-print(entropia,gini )
+        wyniki1 = 0
+        wyniki2 = 0
+        wyniki3 = []
+        wyniki4 = []
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 335723)
+        Tree = tree.DecisionTreeClassifier(criterion = 'gini',
+                                               random_state = 335723 )  
+        scores = cross_val_score(Tree, X_train, y_train, cv = cv)
+        wyniki1 = scores.mean()
+        Tree = tree.DecisionTreeClassifier(criterion = 'entropy',
+                                               random_state = 335723 )  
+        scores = cross_val_score(Tree, X_train, y_train, cv = cv)
+        wyniki2 = scores.mean()
+        for i in range(1,wart + 1):
+            Tree = tree.DecisionTreeClassifier(max_depth = i,
+                                               random_state =335723 )  
+            scores = cross_val_score(Tree, X_train, y_train, cv = cv)
+            wyniki3.append(scores.mean())
+            Tree = tree.DecisionTreeClassifier(min_samples_leaf = i,
+                                               random_state=335723 )  
+            scores = cross_val_score(Tree, X_train, y_train, cv = cv)
+            wyniki4.append(scores.mean())
+        plt.plot(wart,)
+        return(" Dokładność na podstawie kryt. giniego to:",wyniki1,
+               "\n Dokładność na podstawie kryt. etropii to:", wyniki2, 
+               "\n Dokładność na podstawie głebokości drzewa to:", wyniki3,
+               "\n Dokładność na podstawie min liczby obserwacji w lisciu to:"
+               , wyniki4,
+              )
+    
+    wyniki  = accuracy(8,X,y, 10 )
+print(wyniki)
