d330625ca1c2f70086c70515419c2c9e4ed32ff5
Kajetan Zduńczyk <262837@student.pwr.edu.pl>
2023-10-23 16:43:21 +0200

    final changes

--- hm1/homework1.py
+++ hm1/homework1.py
@@ -1,91 +1,81 @@
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score,KFold, GridSearchCV
 from sklearn import tree
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix,accuracy_score, roc_curve,roc_auc_score, auc, accuracy_score, recall_score, precision_score, f1_score, RocCurveDisplay
 
 X = pd.read_csv("../pw/hm1/X.csv")
 y = pd.read_csv("../pw/hm1/y.csv")
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 335723)
 def accuracy(cv:int,X, y, wart = 0):
-    """Function calculates accuracy
-
-    Args:
-        cv (int): _description_
-        X (_type_): _description_
-        y (_type_): _description_
-        wart (int, optional): _description_. Defaults to 0.
-
-    Returns:
-        _type_: _description_
-    """
     if(cv <5):
         return "Podaj wartosc wieksza niz 5"
     else:
-        wyniki1 = 0
-        wyniki2 = 0
-        wyniki3 = []
-        wyniki4 = []
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 335723)
+        wyniki_train = []
+        wyniki_test = []
+        
         Tree = tree.DecisionTreeClassifier(criterion = 'gini',
-                                               random_state = 335723 )  
-        scores = cross_val_score(Tree, X_train, y_train, cv = cv)
-        wyniki1 = scores.mean()
+                                               random_state = 335723 ) 
+        kf = KFold(n_splits = cv, random_state =335723, shuffle = True )
+        scores = cross_val_score(Tree, X_train, y_train, cv = kf)
+        wyniki_train.append(scores.mean())
+        scores = cross_val_score(Tree, X_test, y_test, cv = kf)
+        wyniki_test.append(scores.mean())
+        
         Tree = tree.DecisionTreeClassifier(criterion = 'entropy',
                                                random_state = 335723 )  
-        scores = cross_val_score(Tree, X_train, y_train, cv = cv)
-        wyniki2 = scores.mean()
+        scores = cross_val_score(Tree, X_train, y_train, cv = kf)
+        wyniki_train.append(scores.mean())
+        scores = cross_val_score(Tree, X_test, y_test, cv = kf)
+        wyniki_test.append(scores.mean())
+        
         for i in range(1,wart + 1):
             Tree = tree.DecisionTreeClassifier(max_depth = i,
-                                               random_state =335723 )  
-            scores = cross_val_score(Tree, X_train, y_train, cv = cv)
-            wyniki3.append(scores.mean())
+                                               random_state =335723 )
+            scores = cross_val_score(Tree, X_train, y_train, cv = kf)
+            wyniki_train.append(scores.mean())
+            scores = cross_val_score(Tree, X_test, y_test, cv = kf)
+            wyniki_test.append(scores.mean())
+            
             Tree = tree.DecisionTreeClassifier(min_samples_leaf = i,
-                                               random_state=335723 )  
-            scores = cross_val_score(Tree, X_train, y_train, cv = cv)
-            wyniki4.append(scores.mean())
-        plt.plot(wart,)
-        return(" Dokładność na podstawie kryt. giniego to:",wyniki1,
-               "\n Dokładność na podstawie kryt. etropii to:", wyniki2, 
-               "\n Dokładność na podstawie głebokości drzewa to:", wyniki3,
-               "\n Dokładność na podstawie min liczby obserwacji w lisciu to:"
-               , wyniki4,)
-    
-wyniki  = accuracy(8,X,y, 10)
-print(wyniki)
+                                               random_state=335723)
+            scores = cross_val_score(Tree, X_train, y_train, cv = kf)
+            wyniki_train.append(scores.mean())
+            scores = cross_val_score(Tree, X_test, y_test, cv = kf)
+            wyniki_test.append(scores.mean())
+            
+    return(wyniki_train,wyniki_test)
+              
+wyniki_train,wyniki_test  = accuracy(8,X,y,25)
+print(wyniki_train,wyniki_test)
 
+dt = tree.DecisionTreeClassifier()
 
-fig, ax = plt.subplots()
+param_grid = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [5, 10, 15, 20],
+    'min_samples_leaf': [5, 10, 15, 20],
+    'min_samples_split': [5, 10, 15, 20]
+}
+kf = KFold(n_splits = 8, random_state =335723, shuffle = True )
+grid_search = GridSearchCV(dt, param_grid, cv=kf, scoring='accuracy', n_jobs=-1)
 
-# List of colors for the lines
-colors = ['r', 'g', 'b','b','b','b','b','b','b','b','b','b',
-          'y','y','y','y','y','y','y','y','y','y']
+grid_search.fit(X_train, y_train)
 
+best_params = grid_search.best_params_
+best_score = grid_search.best_score_
 
-for y, color in zip(wyniki, colors):
-    ax.axhline(y=y, color=color, linestyle='--')
+print("Najlepsze parametry na zbiorze treningowym:", best_params)
+print("Najlepsza dokładność na zbiorze treningowym:", best_score)
 
-# Add labels and a legend (optional)
-ax.set_ylabel('Accuracy')
-ax.set_ylim(0.63, 0.84)
-ax.set_xticklabels([])
-# Display the plot
-plt.show()
+best_dt = tree.DecisionTreeClassifier(**best_params)
+best_dt.fit(X_train, y_train)
+y_pred = best_dt.predict(X_test)
+test_accuracy = accuracy_score(y_test, y_pred)
 
-largest_value = max(wyniki)
-largest_index = wyniki.index(largest_value)
-print(largest_value, largest_index)
+print("Dokładność na zbiorze testowym:", test_accuracy)
 
-
-X = pd.read_csv("C:/Users/Kajetan/Desktop/pw/hm1/X.csv")
-y = pd.read_csv("C:/Users/Kajetan/Desktop/pw/hm1/y.csv")
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state= 335723)
-Tree = tree.DecisionTreeClassifier(criterion = 'entropy',
+Tree = tree.DecisionTreeClassifier(criterion= 'entropy', max_depth= 20, min_samples_leaf= 5, min_samples_split= 5,
                                                random_state = 335723 ) 
 Tree = Tree.fit(X_train, y_train)
 y_pred_test = Tree.predict(X_test)
